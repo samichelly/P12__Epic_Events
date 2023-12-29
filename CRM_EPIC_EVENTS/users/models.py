18d513fbd3f0084de27c091b658ee966ef4d2bcd
samichelly <sami.chelly15@gmail.com>
2023-12-29 22:54:43 +0100

    User Create and Authentification with bugs + click interface v1

--- CRM_EPIC_EVENTS/users/models.py
+++ CRM_EPIC_EVENTS/users/models.py
@@ -1,16 +1,133 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+import bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
 
 
+import sys
+from pathlib import Path
+import enum
 
-class User(AbstractUser):
-    ROLE = [
-    ('sales', 'SALES'),
-    ('support', 'SUPPORT'),
-    ('management', 'MANAGEMENT'),
-]
-    
-    role = models.CharField(max_length=10, choices=ROLE)
-    name = models.CharField(max_length=50)
-    employee_number = models.CharField(max_length=20, unique=True)
-    # email = models.EmailField(unique=True)
+parent_path = Path(__file__).resolve().parent.parent
+sys.path.append(str(parent_path))
+print(parent_path)
+
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    Boolean,
+    Text,
+    Date,
+    DECIMAL,
+    Enum,
+    Sequence,
+)
+from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from CRM_EPIC_EVENTS.settings import DATABASE_URL
+
+# print(DATABASE_URL)
+
+# DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class User_role(enum.Enum):
+    sales = "sales"
+    support = "support"
+    management = "management"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, nullable=False)
+    # _password = Column("password", String, nullable=False)
+    password = Column(String, nullable=False)
+    first_name = Column(String)
+    last_name = Column(String)
+    role = Column(
+        ENUM("sales", "support", "management"),
+        name="user_role",
+        default="support",  # enum.ENUM
+    )
+    role = Column(Enum(User_role), name="user_role", default="support")
+
+    # @hybrid_property
+    # def password(self):
+    #     return self._password
+
+    # @password.setter
+    # def set_password_hach(self, plaintext_password):
+    #     hashed_password = bcrypt.hashpw(
+    #         plaintext_password.encode("utf-8"), bcrypt.gensalt()
+    #     )
+    #     self._password = hashed_password.decode("utf-8")
+
+    # def check_password(self, plaintext_password):
+    #     return bcrypt.checkpw(plaintext_password.encode("utf-8"), self._password.encode("utf-8"))
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    phone = Column(String, nullable=False)
+    company_name = Column(String, nullable=False)
+    creation_date = Column(Date, server_default="now()", nullable=False)
+    last_contact_date = Column(
+        Date, server_default="now()", onupdate="now()", nullable=False
+    )
+    sales_contact_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    sales_contact = relationship("User", backref="customer_sales")
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Contract(Base):
+    __tablename__ = "contracts"
+
+    id = Column(Integer, primary_key=True)
+    total_amount = Column(DECIMAL(10, 2), nullable=False)
+    remaining_amount = Column(DECIMAL(10, 2), nullable=False)
+    creation_date = Column(Date, server_default="now()", nullable=False)
+    is_signed = Column(Boolean, default=False)
+    customer_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
+    management_contact_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    customer = relationship("Customer", backref="contracts")
+    management_contact = relationship("User", backref="contract_management")
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True)
+    event_name = Column(String, nullable=False)
+    event_date_start = Column(DateTime, nullable=False)
+    event_date_end = Column(DateTime, nullable=False)
+    location = Column(String, nullable=False)
+    attendees = Column(Integer, nullable=False)
+    notes = Column(Text, nullable=False)
+    contract_id = Column(Integer, ForeignKey("contracts.id"), nullable=False)
+    support_contact_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    contract = relationship("Contract", backref="event_contract")
+    support_contact = relationship("User", backref="event_support")
+
+
+Base.metadata.create_all(engine)
--- CRM_EPIC_EVENTS/users/views.py
+++ CRM_EPIC_EVENTS/users/views.py
@@ -1,3 +1,152 @@
-from django.shortcuts import render
+import bcrypt
+import click
+from models import User, Customer, Contract, Event, session
+from datetime import datetime, timedelta
 
-# Create your views here.
+
+class Context:
+    def __init__(self):
+        self.user = None  # Stocker l'utilisateur authentifié
+
+
+pass_context = click.make_pass_decorator(Context, ensure=True)
+
+
+@click.group()
+@click.pass_context
+def cli(context):
+    """CRM Epic Events"""
+    context.obj = Context()
+
+
+@cli.command()
+@pass_context
+def login(context):
+    """Authenticate a user."""
+    email = click.prompt("Email", type=str)
+    password = click.prompt("Password", type=str, hide_input=True)
+
+    user = authenticate_user(email, password)
+
+    if user:
+        print(f"Authentication successful for user with email {user.email}")
+        context.obj.user = user
+    else:
+        print("Authentication failed.")
+
+
+@cli.command()
+def input_new_user():
+    """Create a new user."""
+    click.echo("Creating a new user:")
+    email = click.prompt("Email", type=str)
+    password = click.prompt(
+        "Password", type=str, hide_input=True, confirmation_prompt=True
+    )
+    first_name = click.prompt("First Name", type=str)
+    last_name = click.prompt("Last Name", type=str)
+    role = click.prompt("Role", type=click.Choice(["sales", "support", "management"]))
+
+    create_user(email, password, first_name, last_name, role)
+    print("User created successfully.")
+
+
+def authenticate_user(email, password):
+    user = session.query(User).filter_by(email=email, password=password).first()
+
+    if user:
+        # if user and bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+        print(f"Successful login for user with email {email}")
+        return user
+    else:
+        print(f"Login failed for user with email {email}")
+        return False
+
+
+@cli.command()
+def create_user(email, password, first_name, last_name, role):
+    """Create a new user."""
+    new_user = User(
+        email=email,
+        password=password,
+        first_name=first_name,
+        last_name=last_name,
+        role=role,
+    )
+
+    session.add(new_user)
+    session.commit()
+    print("User created successfully")
+    return
+
+
+@cli.group()
+@pass_context
+def authenticated_users(context):
+    """Commands for authenticated users"""
+    pass
+
+
+@authenticated_users.command()
+def list_customers():
+    """List all customers."""
+    customers = session.query(Customer).all()
+
+    if not customers:
+        print("No customers found.")
+        return
+
+    print("List of customers:")
+    for customer in customers:
+        print(f"{customer.first_name} {customer.last_name} - {customer.email}")
+
+
+@authenticated_users.command()
+def list_contracts():
+    """List all contracts."""
+    # ... (code to list all contracts)
+
+
+@authenticated_users.command()
+def list_events():
+    """List all events."""
+    # ... (code to list all events)
+
+
+@authenticated_users.command()
+def input_new_customer():
+    """Create a new customer."""
+    click.echo("Creating a new customer:")
+    first_name = click.prompt("Customer First Name", type=str)
+    last_name = click.prompt("Customer Last Name", type=str)
+    email = click.prompt("Customer Email", type=str)
+    phone = click.prompt("Customer Phone", type=str)
+    company_name = click.prompt("Customer Company Name", type=str)
+
+    create_customer(first_name, last_name, email, phone, company_name)
+    print("Customer created successfully.")
+
+    # return first_name, last_name, email, phone, company_name
+
+
+@click.command()
+def create_customer(first_name, last_name, email, phone, company_name):
+    """Create a new customer."""
+    new_customer = Customer(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        phone=phone,
+        company_name=company_name,
+        # sales_contact=new_user,
+    )
+
+    session.add(new_customer)
+    session.commit()
+    print("Customer created successfully")
+
+
+cli.add_command(authenticated_users)
+
+if __name__ == "__main__":
+    cli()
